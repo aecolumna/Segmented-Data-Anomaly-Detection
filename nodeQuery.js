25082d19c8a3bc56f1716839367d13b72791a7d9
CJenkins81 <60165321+CJenkins81@users.noreply.github.com>
2020-02-11 12:12:39 -0500

    Looping functionality for downloading.
    Hardcoded at 10second loops of downloading past 12 hours

--- nodeQuery.js
+++ nodeQuery.js
@@ -14,13 +14,14 @@ function getTimeMinimum(interval) {
 /*This successfully pulls the json? from the controller */
 
 function pullData() {
+    //console.log("pullData()");
     var minTime = getTimeMinimum(3600*12); //Min time is the UNIX time interval seconds previously
     var options = {
         'method': 'POST',
         'url': 'https://analytics.api.appdynamics.com/events/query?limit=10000&start=' + minTime,
         'headers': {
-            'X-Events-API-AccountName': '',
-            'X-Events-API-Key': '',
+            'X-Events-API-AccountName': 'appdmsu_c1887a44-cf00-4a84-8fa7-10a24c6638b1',
+            'X-Events-API-Key': 'f774c677-a969-4401-9d72-fbed038778ba',
             'Content-Type': 'application/vnd.appd.events+text;v=2',
             'Accept': 'application/vnd.appd.events+json;v=2'
         },
@@ -40,9 +41,9 @@ function pullData() {
 
 
 function pullDataLoop(frequencyms) {
-    console.log("pullDataLoop() frequency: " + frequencyms);
     pullData();
-    setTimeout(pullDataLoop,frequencyms,'frequencyms');
+    console.log("pullDataLoop() frequency: " + frequencyms);
+    setTimeout(pullDataLoop,frequencyms,frequencyms);
 }
 
 pullDataLoop(10000);
