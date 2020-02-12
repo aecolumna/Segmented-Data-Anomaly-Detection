3b226d145f6b6f257ae91ef7856ccf0f668aac5b
John W <john@wagenmaker.net>
2020-02-11 22:17:35 -0500

    Update data_sanitizer.py
    Made a Sanitized object that allows you to get a pd DataFrame, np array, or csv. Also added type dict tracking.

--- data_sanitizer.py
+++ data_sanitizer.py
@@ -3,27 +3,63 @@ import pandas as pd
 import csv
 import json
 
-def clean_csv(csv_filepath):
+class Sanitized:
+    def __init__(self, df, array, td):
+        self.pd_df = df
+        self.np_array = array
+        self.type_dict = td
+
+    def as_pd_df(self):
+        return self.pd_df
+
+    def as_np_array(self):
+        return self.np_array
+
+    def get_type_dict(self):
+        return self.type_dict
+
+    def to_file(self, outfile="sanitized_output.csv"):
+        self.pd_df.to_csv(outfile)
+
+
+def sanitize_csv(csv_filepath="AppDynamicsSearchResults.csv"):
     df = pd.read_csv(csv_filepath, header="infer")
     good_columns = []
-    unique_cols = []
-    for col in df.columns:
-        if len(np.unique(df[[col]].values)) != 1 and (not (type(df[col].iloc[0]) == str and "[" in str(df[col].iloc[0]))) :
 
+    #remove columns that are all the same and are unsplit arrays
+    for col in df.columns:
+        if len(np.unique(df[[col]].values)) != 1 and (not (type(df[col].iloc[0]) == str and "[" in str(df[col].iloc[0]))):
             good_columns.append(col)
 
     df = pd.DataFrame(df, columns=good_columns)
     good_columns = []
+    #remove nans
     for col in df.columns:
-        print(col, isinstance(df[col].iloc[0], pd.Series))
-        print()
+        #print(col, isinstance(df[col].iloc[0], pd.Series))
+        #print()
         if str(df[col].iloc[0]) != "nan":
             good_columns.append(col)
 
-    new_table = pd.DataFrame(df, columns=good_columns)
+    #get types
+    type_dict = {}
+    for col in df.columns:
+        if df[col].dtype == np.float64 or df[col].dtype == np.int64:
+            type_dict[col] = "float"
+        elif df[col].dtype == np.object:
+            type_dict[col] = "str"
+        else:
+            raise Exception(f"Unexpected type in data sanitizer: {df[col].dtype}")
 
-    new_table.to_csv("sanitized_output.csv")
+    new_df = pd.DataFrame(df, columns=good_columns)
+    new_array = pd.DataFrame(df).to_numpy()
 
-if __name__ == "__main__()":
-    clean_csv("AppDynamicsSearchResults")
+    return Sanitized(new_df, new_array, type_dict)
+
+
+
+
+if __name__ == "__main__":
+    sanitize_csv("AppDynamicsSearchResults.csv")
+
+
 
