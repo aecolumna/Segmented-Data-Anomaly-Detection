62fb08ae32204ef2b42c039d972549c797d776d8
Columna, Andres <columnaa@msu.edu>
2020-04-04 13:20:59 -0400

    update for john to debug

--- js_integration.py
+++ js_integration.py
@@ -1,6 +1,7 @@
 from data_sanitizer import sanitize_json
 import sys
 import json
+from donut import make_donut_javascript
 
 
 if __name__ == '__main__':
@@ -8,5 +9,5 @@ if __name__ == '__main__':
     raw_data_string = json.dumps(json.load(sys.stdin))
     print(raw_data_string)
     sanitized_data_string = sanitize_json(raw_data_string).to_json_string()
-
+    make_donut_javascript()
     print(sanitized_data_string)
